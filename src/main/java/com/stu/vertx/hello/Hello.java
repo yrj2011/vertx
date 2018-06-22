a525fc43b8ce2bddbbf81ff1ffb9140a39d1f039
kgh <18366131816@163.com>
2018-06-22 17:26:13 +0800

    实现一个最简单的HttpServer

--- src/main/java/com/stu/vertx/hello/Hello.java
+++ src/main/java/com/stu/vertx/hello/Hello.java
@@ -1,5 +1,30 @@
 package com.stu.vertx.hello;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+
 public class Hello {
+	
+	public static void main(String[] args) {
+		
+		Vertx vertx = Vertx.vertx();
+		
+		HttpServer server = vertx.createHttpServer();
+		
+		server.requestHandler(request -> {
+			// 获取到response对象
+			HttpServerResponse response = request.response();
+			
+			// 设置响应头
+			response.putHeader("Content-type", "text/html;charset=utf-8");
+			
+			// 响应数据
+			response.end("Hello World");
+			
+		});
+		
+		server.listen(8888);
+	}
 
 }
