3daf8daced86a1bb99a025cb90306151486389d0
kgh <18366131816@163.com>
2018-06-22 17:31:05 +0800

    完善注释

--- src/main/java/com/stu/vertx/hello/Hello.java
+++ src/main/java/com/stu/vertx/hello/Hello.java
@@ -4,12 +4,19 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 
+/**
+ * Vertx 创建一个最简单的HttpServer，当用户请求时返回Hello World
+ * 
+ * @author lenovo
+ *
+ */
 public class Hello {
 	
 	public static void main(String[] args) {
 		
 		Vertx vertx = Vertx.vertx();
 		
+		// 创建一个HttpServer
 		HttpServer server = vertx.createHttpServer();
 		
 		server.requestHandler(request -> {
