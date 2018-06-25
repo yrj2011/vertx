bef776f3dc76b66a39a59b7ab1311aa04c9fa698
kgh <18366131816@163.com>
2018-06-25 16:54:54 +0800

    修改类名

---
+++ src/main/java/com/stu/vertx/hello/MyHttpServer.java
@@ -0,0 +1,37 @@
+package com.stu.vertx.hello;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+
+/**
+ * Vertx 创建一个最简单的HttpServer，当用户请求时返回Hello World
+ * 
+ * @author lenovo
+ *
+ */
+public class MyHttpServer {
+	
+	public static void main(String[] args) {
+		
+		Vertx vertx = Vertx.vertx();
+		
+		// 创建一个HttpServer
+		HttpServer server = vertx.createHttpServer();
+		
+		server.requestHandler(request -> {
+			// 获取到response对象
+			HttpServerResponse response = request.response();
+			
+			// 设置响应头
+			response.putHeader("Content-type", "text/html;charset=utf-8");
+			
+			// 响应数据
+			response.end("Hello World");
+			
+		});
+		
+		server.listen(8888);
+	}
+
+}
