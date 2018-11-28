725fdcf80754a90f84d1ec0596f6868c2ef6ca88
lenovo <lenovo@lenovo-PC>
2018-11-28 16:59:21 +0800

    新增Vertx计时代码演示

---
+++ src/main/java/com/stu/vertx/timer/VertxTimer.java
@@ -0,0 +1,38 @@
+package com.stu.vertx.timer;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+
+/**
+ * 定时功能
+ * 
+ * @author lenovo
+ *
+ */
+public class VertxTimer extends AbstractVerticle {
+
+	@Override
+	public void start() throws Exception {
+
+		// 计时执行一次
+		long id = vertx.setTimer(3000, d -> {
+			System.out.println("时间到");
+		});
+
+		// 设置间隔时间，每隔一段时间执行一次
+		long id2 = vertx.setPeriodic(1000, aaa -> {
+			System.out.println("时间又到了");
+		});
+		
+		// 取消定时
+		vertx.cancelTimer(id);
+		vertx.cancelTimer(id2);
+		
+	}
+
+	public static void main(String[] args) {
+		Vertx vertx = Vertx.vertx();
+		vertx.deployVerticle(new VertxTimer());
+	}
+
+}
