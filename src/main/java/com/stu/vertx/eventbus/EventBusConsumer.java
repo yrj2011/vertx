d20ddd2b89cdbd21bb4d812bc80d108e616a4dee
lenovo <lenovo@lenovo-PC>
2018-11-29 14:51:27 +0800

    代码优化

--- src/main/java/com/stu/vertx/eventbus/EventBusConsumer.java
+++ src/main/java/com/stu/vertx/eventbus/EventBusConsumer.java
@@ -11,6 +11,7 @@ import io.vertx.core.eventbus.EventBus;
  */
 public class EventBusConsumer extends AbstractVerticle {
 
+	// 使用名字可以标识多个不同的消费者
 	private String name;
 
 	public EventBusConsumer(String name) {
@@ -20,13 +21,19 @@ public class EventBusConsumer extends AbstractVerticle {
 	@Override
 	public void start() throws Exception {
 		EventBus eb = vertx.eventBus();
-
+		
 		// 消费消息
 		eb.consumer("hello", handle -> {
 			String msg = (String) handle.body();
 			String cType = handle.headers().get("Content-Type");
 			System.out.println(name + " 接收到的消息为：" + msg + " 头信息：" + cType);
 		});
+		
+		// 第二种写法
+//		MessageConsumer<String> consumer = eb.consumer("hello");
+//		consumer.handler(handler->{
+//			
+//		});
 	}
 
 }
