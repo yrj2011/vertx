e25c7fa5d1cdf6122455f3543f52921cd45d1c53
lenovo <lenovo@lenovo-PC>
2018-12-03 16:36:39 +0800

    修改

--- src/main/java/com/stu/vertx/jdbc/JdbcUtils.java
+++ src/main/java/com/stu/vertx/jdbc/JdbcUtils.java
@@ -10,10 +10,10 @@ public class JdbcUtils {
 
 	public JdbcUtils(Vertx vertx) {
 		JsonObject dbConfig = new JsonObject();
-		dbConfig.put("url", "jdbc:mysql://192.168.40.66:3306/md5");
+		dbConfig.put("url", "jdbc:mysql://192.168.40.66:3306/test");
 		dbConfig.put("driver_class", "com.mysql.jdbc.Driver");
-		dbConfig.put("user", "zabbix");
-		dbConfig.put("password", "Zabbix8#Zookeeper");
+		dbConfig.put("user", "xxxx");
+		dbConfig.put("password", "xxxx");
 
 		dbClient = JDBCClient.createShared(vertx, dbConfig);
 	}
