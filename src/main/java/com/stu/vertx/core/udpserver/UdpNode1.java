0b9c48031b2dce8c7a1c9e257a48674394a762ac
lenovo <lenovo@lenovo-PC>
2018-12-04 13:53:00 +0800

    UDP 演示

---
+++ src/main/java/com/stu/vertx/core/udpserver/UdpNode1.java
@@ -0,0 +1,34 @@
+package com.stu.vertx.core.udpserver;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.datagram.DatagramSocket;
+import io.vertx.core.datagram.DatagramSocketOptions;
+
+/**
+ * 接收数据
+ * 
+ * @author lenovo
+ *
+ */
+public class UdpNode1 extends AbstractVerticle {
+
+	@Override
+	public void start() throws Exception {
+		DatagramSocket socket = vertx.createDatagramSocket(new DatagramSocketOptions());
+
+		socket.listen(1234, "0.0.0.0", asyncResult -> {
+			if (asyncResult.succeeded()) {
+				socket.handler(packet -> {
+					System.out.println("接收到的数据为：" + packet.data());
+				});
+			} else {
+				System.out.println("Listen failed" + asyncResult.cause());
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		Vertx.vertx().deployVerticle(new UdpNode1());
+	}
+}
