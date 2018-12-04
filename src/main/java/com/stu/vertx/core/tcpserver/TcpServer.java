8c759ed0f724fcffee5828c85dc026fc10336330
lenovo <lenovo@lenovo-PC>
2018-12-04 11:24:18 +0800

    新增Vertx实现tcp客户端

--- src/main/java/com/stu/vertx/core/tcpserver/TcpServer.java
+++ src/main/java/com/stu/vertx/core/tcpserver/TcpServer.java
@@ -21,18 +21,20 @@ public class TcpServer extends AbstractVerticle {
 
 		// 处理连接请求
 		server.connectHandler(socket -> {
-			
+
 			socket.handler(buffer -> {
 				// 在这里应该解析报文，封装为协议对象，并找到响应的处理类，得到处理结果，并响应
 				System.out.println("接收到的数据为：" + buffer.toString());
-				socket.write(Buffer.buffer("Server Knonw"));
+				
+				// 按照协议响应给客户端
+				socket.write(Buffer.buffer("Server Received"));
 			});
 
 			// 监听客户端的退出连接
 			socket.closeHandler(close -> {
 				System.out.println("客户端退出连接");
 			});
-			
+
 		});
 
 		// 监听端口
