650779e0f3244d4eef333d44e0e07f1a2d2e1f92
lenovo <lenovo@lenovo-PC>
2018-12-12 09:25:00 +0800

    简单的tcp实现的时间服务器演示

---
+++ src/main/java/com/stu/vertx/core/tcpserver/TimeClient.java
@@ -0,0 +1,41 @@
+package com.stu.vertx.core.tcpserver;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetSocket;
+
+/**
+ * TCP时间客户端模拟
+ * 
+ * @author lenovo
+ *
+ */
+public class TimeClient extends AbstractVerticle {
+
+	@Override
+	public void start() throws Exception {
+		NetClient client = vertx.createNetClient();
+
+		client.connect(123, "localhost", socket -> {
+			if (socket.succeeded()) {
+				NetSocket s = socket.result();
+				s.write(Buffer.buffer("QUERY TIME ORDER"));
+				s.handler(buffer -> {
+					System.out.println("接收到的时间为：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+							.format(new Date(Long.parseLong(buffer.toString()))));
+				});
+			} else {
+				System.out.println("连接服务器失败");
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		Vertx.vertx().deployVerticle(new TimeClient());
+	}
+}
