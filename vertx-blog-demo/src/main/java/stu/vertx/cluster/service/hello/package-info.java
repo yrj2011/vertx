a33e4c01862b5c8075ddea86fda7562f17e6ba06
孔冠华 <18366131816@163.com>
2019-01-28 23:43:00 +0800

    改变包名

--- vertx-blog-demo/src/main/java/stu/vertx/cluster/service/hello/package-info.java
+++ vertx-blog-demo/src/main/java/stu/vertx/cluster/service/hello/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(groupPackage = "com.stu.vertx.cluster.service.hello", name = "FirstVerticle")
+@ModuleGen(groupPackage = "stu.vertx.cluster.service.hello", name = "FirstVerticle")
 package stu.vertx.cluster.service.hello;
 
 import io.vertx.codegen.annotations.ModuleGen;
