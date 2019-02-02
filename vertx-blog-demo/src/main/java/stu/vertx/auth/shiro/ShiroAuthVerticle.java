251f8a76b661b5e2c44a9e1d603f068a1ed457a6
孔冠华 <18366131816@163.com>
2019-02-02 21:03:46 +0800

    shiro认证演示

---
+++ vertx-blog-demo/src/main/java/stu/vertx/auth/shiro/ShiroAuthVerticle.java
@@ -0,0 +1,43 @@
+package stu.vertx.auth.shiro;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.shiro.ShiroAuth;
+import io.vertx.ext.auth.shiro.ShiroAuthOptions;
+import io.vertx.ext.auth.shiro.ShiroAuthRealmType;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * 使用shiro实现认证和授权的演示案例
+ *
+ * @author lenovo
+ */
+public class ShiroAuthVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        JsonObject config = new JsonObject().put("properties_path", "classpath:auth.properties");
+        ShiroAuth provider = ShiroAuth.create(vertx, new ShiroAuthOptions().setType(ShiroAuthRealmType.PROPERTIES).setConfig(config));
+
+        JsonObject data = new JsonObject()
+                .put("username", "admin")
+                .put("password", "admin");
+        provider.authenticate(data, auth -> {
+            if (auth.succeeded()) {
+                System.out.println("认证成功");
+            } else {
+                System.out.println("认证失败");
+            }
+        });
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    public static void main(String[] args) {
+        Vertx.vertx().deployVerticle(new ShiroAuthVerticle());
+    }
+}
