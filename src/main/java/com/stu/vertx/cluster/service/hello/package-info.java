2222239330f97a223b37210e76e13ac614a8ec54
lenovo <18366131816@163.com>
2019-02-02 09:02:00 +0800

    x

--- src/main/java/com/stu/vertx/cluster/service/hello/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(groupPackage = "com.stu.vertx.cluster.service.hello", name = "FirstVerticle")
-package com.stu.vertx.cluster.service.hello;
-
-import io.vertx.codegen.annotations.ModuleGen;
