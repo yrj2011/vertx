7e161081644e8541b2fa3b840b5b2a0e7e5d4bb3
孔冠华 <18366131816@163.com>
2019-03-24 16:51:43 +0800

    增加属性，方便构建链

--- vertx-framework/vertx-framework-common/src/main/java/com/kingh/vertx/common/anno/Chain.java
+++ vertx-framework/vertx-framework-common/src/main/java/com/kingh/vertx/common/anno/Chain.java
@@ -1,5 +1,7 @@
 package com.kingh.vertx.common.anno;
 
+import io.vertx.core.http.HttpMethod;
+
 import java.lang.annotation.*;
 
 /**
@@ -13,4 +15,47 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Chain {
+
+    /**
+     * 链的名字
+     *
+     * @return
+     */
+    String name() default "";
+
+    /**
+     * 链的描述
+     *
+     * @return
+     */
+    String description() default "";
+
+    /**
+     * 处理的请求路径
+     *
+     * @return
+     */
+    String path() default "";
+
+    /**
+     * 链是否可用
+     *
+     * @return
+     */
+    boolean avaiable() default false;
+
+    /**
+     * 是否是普通链，特殊链不调用服务
+     *
+     * @return
+     */
+    boolean general() default false;
+
+    /**
+     * 支持的HTTP请求方法，默认为都支持
+     *
+     * @return
+     */
+    HttpMethod[] methods() default {};
+
 }
